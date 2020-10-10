45bcacdda3570e60da153b9759df96e2f7f8cf3d
aidabyte <ablinstrubyte@gmail.com>
2020-10-10 16:50:00 -0500

    update files and script

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    var cityArr 
+
+    var apiKey= "&appid=13da59a70b35153b31d2c096606c4719";
+    var searchButton = $("#searchButton")
+    searchButton.on("click", function(e) {
+        e.preventDefault();
+        var city = $("#searchCity").val();
+        cityArr.push(city)
+        localStorage.setItem("cities", JSON.stringify(cityArr))
+    
+        getStorage();
+
+        getWeather(city);
+    });
+    function getWeather(city) {
+        $("#currentCityName").empty();
+        var queryURL= "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            
+            $(".temperature").html("");
+            $(".humidity").html("");
+            var cityName = response.name;
+           
+
+            var cityP = $("<p>").text(response.name)
+           
+            var icon = $("<img>").attr("src","https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+         
+          
+
+        })
+      
+    }    
+    function getStorage() {
+        $(".city-area").empty();
+        cityArr = JSON.parse(localStorage.getItem("cities")) 
+        
+        var noDuplicates = [...new Set(cityArr)]
+        console.log(noDuplicates)
+        for(var i=0; i<noDuplicates.length; i++) {
+            if (noDuplicates[i].length>0){
+                var cityBtn= $("<button>").text(noDuplicates[i]).addClass("city-btn")
+                $(".city-area").append(cityBtn);  
+            }
+        
+        }
+    }
+    $(document).on("click",".city-btn",function() {
+       
+       getWeather($(this).text());
+    })
+    getStorage();
 });
 
 
 
-var apiKey= "&appid=13da59a70b35153b31d2c096606c4719";
-
-var queryURL= "api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
-
-var city = $("searchCity").val();
-console.log("searchCity")
 
 
-$.ajax({
-    url:queryURL,
-    method: "GET"
-})
-.then(function(response) {
-    console.log(response)
-})
+
+
+
+
+
