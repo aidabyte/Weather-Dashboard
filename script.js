9ac141e37e8515b6ca007f33694fa152ec5c14e6
aidabyte <ablinstrubyte@gmail.com>
2020-10-10 21:18:29 -0500

    final changes

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    
+
     var cityArr 
-    
+
+
+    $("currentDay").text(moment());
+    setInterval(displayTodaysDate)
+
+    currentDate = $("#currentDay");
+    displayTodaysDate();
+
+    function displayTodaysDate() {
+        currentDate.text(moment().format('L'));
+    }
+});
 
     var apiKey= "&appid=13da59a70b35153b31d2c096606c4719";
     var searchButton = $("#searchButton")
     searchButton.on("click", function(e) {
         e.preventDefault();
         var city = $("#searchCity").val();
-        // cityArr.push(city);
-        localStorage.setItem("cities", JSON.stringify(cityArr));
-
+        cityArr.push(city)
+        localStorage.setItem("cities", JSON.stringify(cityArr))
     
         getStorage();
 
         getWeather(city);
     });
-
-    
     function getWeather(city) {
         $("#currentCityName").empty();
         var queryURL= "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
@@ -27,23 +35,22 @@ $( document ).ready(function() {
             method: "GET"
         })
         .then(function(response) {
-            
+            console.log(response)
             $(".temperature").html("");
             $(".humidity").html("");
             $(".windSpeed").html("");
             var cityName = response.name;
-            console.log(response.name)
            
 
-            var cityP = $("<p>").text(response.name);
-           
+            var cityP = $("<p>").text(response.name)
+            
             var icon = $("<img>").attr("src","https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
 
             if (cityName.length>1){
                 var humidity = response.main.humidity;
                 $(".humidity").html("Humidity: " + humidity + " % ");
                 var temperature = response.main.temp;
-                temperature = (Math.floor(temperature - 273.15)*1.80+32);
+                var temperature = (Math.floor(temperature - 273.15)*1.80+32);
                 
                 $(".temperature").html("Temperature: " + temperature + "&#8457");
                 var windSpeed = response.wind.speed;
@@ -61,14 +68,14 @@ $( document ).ready(function() {
     function getStorage() {
         $(".city-area").empty();
         cityArr = JSON.parse(localStorage.getItem("cities")) 
-        // console.log(cityArr)
+        
         var noDuplicates = [...new Set(cityArr)]
-        // console.log(noDuplicates)
+        
         for(var i=0; i<noDuplicates.length; i++) {
             if (noDuplicates[i].length>0){
                 var cityBtn= $("<button>").text(noDuplicates[i]).addClass("city-btn")
                 $(".city-area").append(cityBtn);  
-                $(".city-area").remove("test");
+                
             }
         
         }
@@ -79,13 +86,10 @@ $( document ).ready(function() {
 
 
     $(document).on("click",".city-btn",function() {
-        // console.log($(this).text())
+       
        getWeather($(this).text());
     })
     getStorage();
-
-});
-
 
 
 
